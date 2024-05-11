404289d679b7c764e997bc866f8fb394dcb7359e
fuka0215 <162338606+fuka0215@users.noreply.github.com>
2024-05-11 08:10:53 +0000

    [update]4-27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -14,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully"
-      redirect_to user_path(@user)
+      redirect_to user_path(@user.id)
     else
       render :edit
     end
@@ -31,5 +32,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
+  end
+
 
 end
